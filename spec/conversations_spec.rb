a3a91c28b826ac5281db7778279b5d07a780943f
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-05-22 15:46:24 +0700

    Initital setup

---
+++ spec/conversations_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Conversations do
+  it "has a version number" do
+    expect(Conversations::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
